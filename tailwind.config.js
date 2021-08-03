7f933ec4701fce41d9592208e9587b5c40078209
die-perez <pzandie@gmail.com>
2021-08-03 11:10:56 -0700

    daisyui implementation

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    themes: [
+      'black', // first one will be the default theme
+    ],
+  },
+}
