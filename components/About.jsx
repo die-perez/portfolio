b075f2a094cc014f61864ff48d1cc2b7a7b6434f
die-perez <pzandie@gmail.com>
2021-08-03 20:51:52 -0700

    girl icon

--- components/About.jsx
+++ components/About.jsx
@@ -7,8 +7,8 @@ export const About = () => {
         <section className={styles.about}>
             <div styles={{backgroundColor: "black"}} className={styles.sectionLook}>
                 <h1 className={styles.aboutHeading}>About</h1>
-                <Image src={Me} alt="Andie Perez" width= {250} height={250} className={styles.me}/>
-                <p>I am a Full-Stack Engineer with an eye for design and an appetite for problem-solving. Beautiful websites pique my interest, which is why I strive to learn new skills that help with workflow and creativity every day.<br />Find me altering my clothing or dying my hair when not programming!
+                <Image src={Me} alt="Andie Perez" width= {250} height={250} className={styles.me} />
+                <p style={{marginTop:"50px"}}>I am a Full-Stack Engineer with an eye for design and an appetite for problem-solving. Beautiful websites pique my interest, which is why I strive to learn new skills that help with workflow and creativity every day.<br />Find me altering my clothing or dying my hair when not programming!
                 </p> 
             </div>
         </section>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,24 +1,27 @@
 /* eslint-disable react/no-unknown-property */
+import styles from '../styles/Home.module.css'
 
 
 export const Navbar = () => {
     return (
         <div className="navbar sticky top-0 mb-5 shadow-lg bg-neutral text-neutral-content rounded-box min-w-full z-10" style={{backgroundColor: "black"}}>
             <div className="px-2 mx-2 navbar-start">
-                <span className="text-lg font-bold">
+                <div className={styles.iconHolder}>
+                </div>
+                {/* <span className="text-lg font-bold">
                         Andie
-                </span>
+                </span> */}
             </div> 
             <div className="hidden px-2 mx-2 navbar-center lg:flex">
                 <div className="flex items-stretch">
-                    <a className="btn btn-ghost btn-sm rounded-btn text-white">
+                    {/* <a className="btn btn-ghost btn-sm rounded-btn text-white">
                             Home
-                            </a> 
-                    <a className="btn btn-ghost btn-sm rounded-btn text-white">
-                            Portfolio
-                            </a> 
+                            </a>  */}
                     <a className="btn btn-ghost btn-sm rounded-btn text-white">
                             About
+                            </a> 
+                    <a className="btn btn-ghost btn-sm rounded-btn text-white">
+                            Projects
                             </a> 
                     <a className="btn btn-ghost btn-sm rounded-btn text-white">
                             Contact
