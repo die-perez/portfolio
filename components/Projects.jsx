56fe8d29bcb48171f7a99028a2905237ccd744e2
die-perez <pzandie@gmail.com>
2021-08-09 12:35:27 -0700

    added media breakpoints for larger screens

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -3,22 +3,24 @@ import Image from 'next/image'
 import burnwitch from '../public/images/burnwitch.png'
 import vamp from '../public/images/vamp.png'
 import viberant from '../public/images/viberant.png'
-import Link from 'next/link'
 
 export const Projects = () => {
     return (
         <section id="projects" className={styles.about}>
             <div styles={{backgroundColor: "black"}} className={styles.sectionLook}>
                 <h1 className={styles.aboutHeading}>Projects</h1>
-                    <div className="container flex-wrap">
-                    <div className="card text-center shadow-2xl flex-1">
+                    {/* <div className="container flex-wrap"> */}
+                    <div className={styles.projectInfo}>
+                    <div className="card text-center shadow-2xl ">
                         <figure className="px-10 pt-10">
                             <Image src={burnwitch}
-                            alt="hangman style game" />
+                            alt="hangman style game" 
+                            layout="responsive"
+                            />
                         </figure> 
                         <div className="card-body">
                             <h2 className="card-title">burnwitch</h2>  
-                            <p>Front-end based game built with vanilla Javascript, CSS and HTML. It’s hangman with a dark twist, where the user can save a witch from burning by guessing the word correctly! Fully styled with music and sound effects to enhance user experience.</p>
+                            <p className={styles.aboutPar}>Front-end based game built with vanilla Javascript, CSS and HTML. It’s hangman with a dark twist, where the user can save a witch from burning by guessing the word correctly! Fully styled with music and sound effects to enhance user experience.</p>
                             <div className="justify-center card-actions">
                                 <a href="https://github.com/die-perez/burnwitch"
                                     target="_blank"
@@ -36,11 +38,13 @@ export const Projects = () => {
                     <div className="card text-center shadow-2xl flex-1">
                         <figure className="px-10 pt-10">
                             <Image src={vamp}
-                            alt="hangman style game" />
+                            alt="hangman style game" 
+                            layout="responsive"
+                            />
                         </figure> 
                         <div className="card-body">
                             <h2 className="card-title">vamp</h2>
-                            <p>Full-stack web application using The Makeup API that allows users to look up products based on type and/or brand. Developed to merge multiple makeup product review sources into one, while enabling users to review and rate products. Established backend with Sequelize to create user and product table relationships. Rendered API data from The Makeup API, integrating RESTful routing with Express.</p>  
+                            <p className={styles.aboutPar}>Full-stack web application using The Makeup API that allows users to look up products based on type and/or brand. Developed to merge multiple makeup product review sources into one, while enabling users to review and rate products. Established backend with Sequelize to create user and product table relationships. Rendered API data from The Makeup API, integrating RESTful routing with Express.</p>  
                             <div className="justify-center card-actions">
                                 <a href="https://github.com/die-perez/vamp"
                                     target="_blank"
@@ -58,11 +62,13 @@ export const Projects = () => {
                     <div className="card text-center shadow-2xl flex-1">
                         <figure className="px-10 pt-10">
                             <Image src={viberant}
-                            alt="hangman style game" />
+                            alt="hangman style game" 
+                            layout="responsive"
+                            />
                         </figure> 
                         <div className="card-body">
                             <h2 className="card-title">viberant</h2>  
-                            <p>Full-stack web application mimicking Craiglsist’s personal section for a younger generation. Allows users to post something to do and specify how many people can attend. Users can then join eachother’s events. User authentication utilizing jwt tokens, mongoDB back-end with relational schemas connecting users and events. React front-end that breaks down several components and imports them where required. </p>
+                            <p className={styles.aboutPar}>Full-stack web application mimicking Craiglsist’s personal section for a younger generation. Allows users to post something to do and specify how many people can attend. Users can then join eachother’s events. User authentication utilizing jwt tokens, mongoDB back-end with relational schemas connecting users and events. React front-end that breaks down several components and imports them where required. </p>
                             <div className="justify-center card-actions">
                                 <a href="https://github.com/die-perez/mern-auth-client-1"
                                     target="_blank"
@@ -78,7 +84,8 @@ export const Projects = () => {
                         </div>
                     </div> 
                 </div>
-            </div>
+                </div>
+            {/* </div> */}
             <div className={styles.space}></div>
         </section>
     )
