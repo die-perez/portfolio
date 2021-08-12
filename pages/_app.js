ec7beab8bfa4b4f2a82dddf9f8b8a9c8388d8ea4
die-perez <pzandie@gmail.com>
2021-08-12 12:48:03 -0700

    google analytics added to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
+import Router from 'next/router'
+import withGA from 'next-ga'
+
+
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -10,4 +14,4 @@ function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default withGA("G-T4NYMLDXZT", Router)(MyApp)
